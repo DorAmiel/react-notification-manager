164ae0034c91526f6c1f83cd4a2ea7d14bc25348
DorAmiel <doradir@gmail.com>
2022-02-03 20:29:56 +0200

    next step

--- src/components/CreateNotification.jsx
+++ src/components/CreateNotification.jsx
@@ -1,6 +1,44 @@
-const CreateNotification =()=>{
-    return(
-        <p>CreateNotification</p>
+import { makeStyles } from '@mui/styles';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
+
+
+const useStyles = makeStyles({
+    root:{
+        width:'100vw',
+        display:'flex',
+        justifyContent:'center'
+    },
+    paper: {
+        padding:10,
+        marginTop: 200,
+        width: 300,
+        height: 200,
+        textAlign:'center',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'space-around'
+    }
+});
+const CreateNotification = () => {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.root}>
+        <Paper elevation={3} className={classes.paper}>
+        <Typography variant="h6" component="div" >
+                        Create New Notification
+        </Typography>
+        <TextField id="outlined-basic" label="Message" variant="outlined" />
+        <Button variant="outlined">Create</Button>
+
+        </Paper>
+        </div>
+ 
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,54 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import Badge from '@mui/material/Badge';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+
+const useStyles = makeStyles({
+    links: {
+        width: '30%',
+        display:'flex',
+        justifyContent: 'space-between',
+
+    },
+    link: {
+        color: "white",
+        textDecoration: "none",
+        fontSize: 18
+    }
+});
 
 const Navbar = () => {
+
+    const classes = useStyles()
+    const notifications = useSelector(state => state.navbar.notifications)
+    console.log(notifications)
+
     return (
-        <Box sx={{ flexGrow: 1 , height: 50}}>
+        <Box sx={{ flexGrow: 1, height: 50 }}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Notifications System
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <div className={classes.links}>
+                        <Link
+                            className={classes.link}
+                            to="/home"
+                        >Home</Link>
+                        <Link
+                            className={classes.link}
+                            to="/CreateNotification"
+                        >Create</Link>
+                        <Badge badgeContent={notifications.length} color="error">
+                            <NotificationsIcon />
+                        </Badge>
+                    </div>
                 </Toolbar>
             </AppBar>
         </Box>
